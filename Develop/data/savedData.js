5e48ff1e7f7cd31370be42a0306e9a7ede97c55c
shakib <shakib1122@gmail.com>
2020-07-26 23:15:37 +1000

    second upload

---
+++ Develop/data/savedData.js
@@ -0,0 +1,3 @@
+let savedData = [];
+
+module.exports = savedData
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,21 +1,35 @@
-const noteTask = require("../public/assets/js/index")
+
+let savedData = require("../data/savedData")
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
-        res.json(noteTask);
-    })
+        res.json(savedData)
+    });
+
+    app.post("/api/notes", function (req, res) {
+        savedData.push(req.body);
+        res.json("saved");
+    });
+
+    app.delete("/api/notes/:index", function (req, res) {
+        var elem = parseInt(req.params.index);
+        var tempjournal = [];
+        for (var i = 0; i < savedData.length; i++) {
+          if (i !== elem) {
+            tempjournal.push(savedData[i]);
+          }
+        }
+        savedData = tempjournal;
+    
+        res.json("delete done");
+        console.log ("deleted")
+      });
 };
 
-app.post("/api/notes", function (req, res) {
-    noteTask.push(req.body);
-    res.json(true);
-});
 
 
 
-// app.post("/api/delete", function (req, res) {
-    
-   
-// });
+
+
 
 
